6c7d9b6f07e079d4085c3301af6ffb9244d21564
YoNgWeI <30373219+YoNgWeI1992@users.noreply.github.com>
2020-12-20 11:11:20 +0800

    创建分支day1-start,并提交

--- day1-start/src/index.js
+++ day1-start/src/index.js
@@ -1,10 +1,15 @@
+import _ from 'lodash';
+
 function component() {
-    var element = document.createElement('div');
-  
-    // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+  var element = document.createElement('div');
+
+  // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  return element;
+}
+
+
+console.log(_);
+document.body.appendChild(component());
